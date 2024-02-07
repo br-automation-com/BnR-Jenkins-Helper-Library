cad7340e24c1ce565a751e08fe2ecc7d3fb4a076
Wesley Buchanan <Wesley.Buchanan@br-automation.com>
2024-02-07 13:51:15 -0500

    remove unecessary scripts

--- vars/CreateInstaller.groovy
+++
@@ -1,3 +0,0 @@
-def call(Map config = [:]){
-    powershell(script: "python '${GetResources()}/scripts/CreateInstaller.py' --project '${config.project}' --name ${config.name} --output '${config.output}' --version '${config.version}'");
-}
