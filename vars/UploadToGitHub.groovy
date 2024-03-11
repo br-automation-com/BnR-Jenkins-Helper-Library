fdb27b5cc891f074ba771c75dd7bd57719ca845f
Wesley Buchanan <Wesley.Buchanan@br-automation.com>
2024-03-11 15:27:56 -0400

    move withCredentials to be inside the shared library

--- vars/UploadToGitHub.groovy
+++ vars/UploadToGitHub.groovy
@@ -1,6 +1,17 @@
 def call(Map config = [:]) {
     def fileName = config.file.trim();
-    def downloadURL = pwsh(returnStdout: true, script: ". '${GetResources()}/scripts/UploadToGitHub.ps1' V${config.version} ${config.organization} ${config.name} '${fileName}'").trim();
-    return "${downloadURL}";
+    def credentialsId = config.credentialId
+    if (config.organization == 'br-na-pm') {
+        credentialsId = 'Jenkins-BR-NA'
+    }
+    else if (config.orginzation == 'br-automation-com') {
+        credentialsId = 'br-automation-com-github'
+    }
+    withCredentials([usernamePassword(credentialsId: "$credentialsId",
+                                          usernameVariable: 'GITHUB_APP',
+                                          passwordVariable: 'GITHUB_ACCESS_TOKEN')])
+    {
+        return pwsh(returnStdout: true, script: ". '${GetResources()}/scripts/UploadToGitHub.ps1' V${config.version} ${config.organization} ${config.name} '${fileName}'").trim();
+    }
 }
     
