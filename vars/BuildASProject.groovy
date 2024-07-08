eeb01a2c776c7b88ffe48c113534c62704f4c542
Wesley Buchanan <wesley.buchanan@br-automation.com>
2024-07-08 13:50:50 -0400

    test

--- vars/BuildASProject.groovy
+++ vars/BuildASProject.groovy
@@ -3,5 +3,8 @@ def call(Map config = [:]) {
     def clean = ''
     if (config.clean == false)
         clean = '--no-clean'
-    powershell(script: "python '${GetResources()}/scripts/ASProjectCompile.py' --project '${config.project}' --configuration ${config.configuration} --maxwarnings ${config.max_warnings} --buildpip ${config.buildpip} ${clean}");
+    
+    powershell(script: 'E:/BrAutomation/AS410/Bin-en/BR.AS.Build.exe "E:/workspace/mapp_Framework_develop_2/BaseProject/mappFramework/mappFramework.apj" -cleanAll -t C:/Temp/Simulation -o C:/Temp/Binaries/Simulation');
+    
+    //powershell(script: "python '${GetResources()}/scripts/ASProjectCompile.py' --project '${config.project}' --configuration ${config.configuration} --maxwarnings ${config.max_warnings} --buildpip ${config.buildpip} ${clean}");
 }
