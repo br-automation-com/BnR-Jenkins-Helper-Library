ab39dc6243206367149c7d3d9e65715476140b1a
Wesley Buchanan <wesley.buchanan@br-automation.com>
2024-07-30 14:03:15 -0400

    Correct default in RunMappViewIntegrationTests

--- vars/RunMappViewIntegrationTests.groovy
+++ vars/RunMappViewIntegrationTests.groovy
@@ -1,6 +1,6 @@
 def call(Map config = [:]){
 	config.configuration = config.configuration  ?: 'Simulation'
-    config.buildArSim = config.buildArSim  ?: True
+    config.buildArSim = config.buildArSim ?: true
     
     if (config.buildArSim)
         powershell(script: "python '${GetResources()}/scripts/CreateArSimInstallation.py' --project '${config.project}' --configuration '${config.configuration}' --simulationDirectory '${config.project}/ArSim'");
