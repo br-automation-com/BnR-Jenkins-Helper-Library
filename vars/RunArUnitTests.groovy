10446813726dfc4bb38df25a2901b1b10933c5f2
Wesley Buchanan <wesley.buchanan@br-automation.com>
2024-11-13 13:18:02 -0500

    remove debug statements

--- vars/RunArUnitTests.groovy
+++ vars/RunArUnitTests.groovy
@@ -6,13 +6,9 @@ def call(Map config = [:]){
     powershell(script: "Remove-Item -Recurse '${config.project}/ArSim'");
     powershell(script: "python '${GetResources()}/scripts/CreateArSimInstallation.py' --project '${config.project}' --configuration '${config.configuration}' --simulationDirectory '${config.project}/ArSim'");
     powershell(script: "python '${GetResources()}/scripts/StartArSim.py' --simulationDirectory '${config.project}/ArSim'");
-    echo "ArSim Started"
-    echo "running command python '${GetResources()}/scripts/RunUnitTests.py' --test ${config.tests} --output '${config.project}/${config.output}' --port ${config.port}"
     powershell(script: "python '${GetResources()}/scripts/RunUnitTests.py' --test ${config.tests} --output '${config.project}/${config.output}' --port ${config.port}");
-    echo "Unittest finished"
     powershell(script: "python '${GetResources()}/scripts/StopArSim.py' --simulationDirectory '${config.project}/ArSim'");
-    echo "ArSim Stoped"
-    powershell(script: "python '${GetResources()}/scripts/ProcessCodeCoverage.py' --project '${config.project}' --config '${config.configuration}'")    
+    powershell(script: "python '${GetResources()}/scripts/ProcessCodeCoverage.py' --project '${config.project}' --config '${config.configuration}'");
     if (findFiles( glob: '**/*.gcov').length > 0) {
         recordCoverage(sourceDirectories: [[path: "$PROJECT_DIR"]], tools: [[parser: 'COBERTURA', pattern: '**/report.xml']])
     }
