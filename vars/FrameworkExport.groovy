f4cb29b80b2d7526f7bcb8a25190dd99151a9d62
Wesley Buchanan <wesley.buchanan@br-automation.com>
2025-01-17 14:59:29 -0500

    add vc4 option to FrameworkExport

--- vars/FrameworkExport.groovy
+++ vars/FrameworkExport.groovy
@@ -1,3 +1,4 @@
 def call(Map config = [:]){
-    powershell(script: "python '${GetResources()}/scripts/Export.py' --project '${config.project}' --config '${config.config}' --output '${config.output}' --physical '${config.physical}'");
+    config.vc4 = config.vc4  ?: 'true'
+    powershell(script: "python '${GetResources()}/scripts/Export.py' --project '${config.project}' --config '${config.config}' --output '${config.output}' --physical '${config.physical}' --vc4 '${config.vc4}'");
 }
